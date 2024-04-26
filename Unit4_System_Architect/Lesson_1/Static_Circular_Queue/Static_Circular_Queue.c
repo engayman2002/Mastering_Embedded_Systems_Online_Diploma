5b46b8b6b242c5bed7e9fa9564abcaf3c77063de
Ayman Elashry <engaymanvip@gmail.com>
2024-04-26 06:24:01 +0300

    Static_Circular_Queue

---
+++ Unit4_System_Architect/Lesson_1/Static_Circular_Queue/Static_Circular_Queue.c
@@ -0,0 +1,164 @@
+#include "Static_Circular_Queue.h"
+
+
+// Hidden Methods
+static Queue_Status Queue_Validation(Queue_Buf_t* My_Queue)
+{
+    if(!My_Queue->base || !My_Queue->front || !My_Queue->rear || My_Queue->length<1)
+    {
+        return QUEUE_NOK;
+    }
+    return QUEUE_OK;
+}
+
+// APIs (Implementation)
+
+Queue_Status Queue_Init      (Queue_Buf_t* My_Queue, element_type* My_Storage, uint32_t length)
+{
+    // For Validation
+    if(My_Storage==NULL || length<1)
+    {
+        return QUEUE_NOK;
+    }
+
+    My_Queue->base=My_Storage;
+    My_Queue->front=My_Storage;
+    My_Queue->rear=My_Storage;
+
+    My_Queue->count=0;
+    My_Queue->length=length;
+    
+    return QUEUE_OK;
+}
+
+Queue_Status Queue_Enqueue   (Queue_Buf_t* My_Queue,element_type item)
+{
+    // For Validation
+    if(Queue_Validation(My_Queue)==QUEUE_NOK)
+    {
+        return QUEUE_NOK;
+    }
+
+    if(Queue_IS_FULL(My_Queue)==QUEUE_FULL)
+    {
+        return QUEUE_FULL;
+    }
+
+    *(My_Queue->rear) = item;
+    
+    if( (My_Queue->base+My_Queue->length-1) == My_Queue->rear )
+    {
+        My_Queue->rear = My_Queue->base;
+    }
+    else
+    {
+        My_Queue->rear++;
+    }
+
+    My_Queue->count++;
+
+    return QUEUE_OK;
+}
+
+Queue_Status Queue_Dequeue   (Queue_Buf_t* My_Queue,element_type* item)
+{
+    // For Validation
+    if(Queue_Validation(My_Queue)==QUEUE_NOK)
+    {
+        return QUEUE_NOK;
+    }
+
+    if(Queue_IS_EMPTY(My_Queue)==QUEUE_EMPTY)
+    {
+        return QUEUE_EMPTY;
+    }
+
+    *item=*(My_Queue->front);
+
+    if((My_Queue->base+My_Queue->length-1) == My_Queue->front)
+    {
+        My_Queue->front = My_Queue->base;
+    }
+    else
+    {
+        My_Queue->front++;
+    }
+
+    My_Queue->count--;
+
+    return QUEUE_OK;
+}
+
+Queue_Status Queue_Front     (Queue_Buf_t* My_Queue,element_type* item)
+{
+    // For Validation
+    if(Queue_Validation(My_Queue)==QUEUE_NOK)
+    {
+        return QUEUE_NOK;
+    }
+
+    if(Queue_IS_EMPTY(My_Queue)==QUEUE_EMPTY)
+    {
+        return QUEUE_EMPTY;
+    }
+
+    *item=*(My_Queue->front);
+
+    return QUEUE_OK;
+}
+
+Queue_Status Queue_Back      (Queue_Buf_t* My_Queue,element_type* item)
+{
+    // For Validation
+    if(Queue_Validation(My_Queue)==QUEUE_NOK)
+    {
+        return QUEUE_NOK;
+    }
+
+    if(Queue_IS_EMPTY(My_Queue)==QUEUE_EMPTY)
+    {
+        return QUEUE_EMPTY;
+    }
+
+    if(My_Queue->rear==My_Queue->base)
+    {
+        *item= *(My_Queue->base+My_Queue->length-1);
+    }
+    else
+    {
+        *item = *(My_Queue->rear-1);
+    }
+
+    return QUEUE_OK;
+}
+
+Queue_Status Queue_Size      (Queue_Buf_t* My_Queue,uint32_t* size)
+{
+    // For Validation
+    if(Queue_Validation(My_Queue)==QUEUE_NOK)
+    {
+        return QUEUE_NOK;
+    }
+
+    *size=My_Queue->count;
+
+    return QUEUE_OK;
+}
+Queue_Status Queue_IS_FULL   (Queue_Buf_t* My_Queue)
+{
+    if(My_Queue->count+1==My_Queue->length)
+    {
+        return QUEUE_FULL;
+    }
+    
+    return QUEUE_NOT_FULL;
+}
+Queue_Status Queue_IS_EMPTY  (Queue_Buf_t* My_Queue)
+{
+    if(My_Queue->count==0)
+    {
+        return QUEUE_EMPTY;
+    }
+
+    return QUEUE_NOT_EMPTY;
+}
---
+++ Unit4_System_Architect/Lesson_1/Static_Circular_Queue/Static_Circular_Queue.h
@@ -0,0 +1,51 @@
+#ifndef Static_Circular_Queue_H_
+#define Static_Circular_Queue_H_
+
+#include "My_Platform_Types.h"
+
+// You must change element_type with your prefered data type.
+#define element_type uint16_t
+
+typedef struct 
+{
+    element_type* base;
+    element_type* front;
+    element_type* rear;
+    
+    uint32_t count;
+    uint32_t length;
+
+}Queue_Buf_t;
+
+// You must declare your static array because it's the storage of your Queue.
+/* Example:
+	element_type My_Queue[100];
+	// Here you created queue with maximum length (100 elements).
+*/
+
+typedef enum
+{
+	QUEUE_NOK,
+	QUEUE_OK,
+	QUEUE_FULL,
+    QUEUE_NOT_FULL,
+	QUEUE_EMPTY,
+    QUEUE_NOT_EMPTY
+}Queue_Status;
+
+// APIs
+extern Queue_Status Queue_Init      (Queue_Buf_t* My_Queue, element_type* My_Storage, uint32_t length);
+
+extern Queue_Status Queue_Enqueue   (Queue_Buf_t* My_Queue,element_type item);
+extern Queue_Status Queue_Dequeue   (Queue_Buf_t* My_Queue,element_type* item);
+
+extern Queue_Status Queue_Front     (Queue_Buf_t* My_Queue,element_type* item);
+extern Queue_Status Queue_Back      (Queue_Buf_t* My_Queue,element_type* item);
+
+extern Queue_Status Queue_Size      (Queue_Buf_t* My_Queue);
+extern Queue_Status Queue_IS_FULL   (Queue_Buf_t* My_Queue);
+extern Queue_Status Queue_IS_EMPTY  (Queue_Buf_t* My_Queue);
+
+
+
+#endif
