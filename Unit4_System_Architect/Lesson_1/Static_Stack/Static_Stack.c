629b1ba6a9144745dfbed975bb0bbff75c04e0b0
Ayman Elashry <engaymanvip@gmail.com>
2024-04-28 03:30:41 +0300

    Fix Errors

--- Unit4_System_Architect/Lesson_1/Static_Stack/Static_Stack.c
+++ Unit4_System_Architect/Lesson_1/Static_Stack/Static_Stack.c
@@ -20,66 +20,49 @@ Stack_Status Stack_Init		(Stack_Buf_t* My_Stack,element_type* My_Storage,uint32_
 Stack_Status Stack_Push		(Stack_Buf_t* My_Stack , element_type item)
 {
     // For Validation
-    if(Stack_IS_FULL==STACK_FULL)
+    if(Stack_IS_FULL(My_Stack)==STACK_FULL)
     {
         return STACK_FULL;
     }
 
     My_Stack->count++;
     *(My_Stack->head)=item;
-    if( (My_Stack->count) < (My_Stack->length) )
-    {
-        My_Stack->head++;
-    }
-    
+
+    My_Stack->head++;
+
     return STACK_OK;
 }
 
 Stack_Status Stack_Pop		(Stack_Buf_t* My_Stack , element_type *item)
 {
     // For Validation
-    if(Stack_IS_EMPTY==STACK_EMPTY)
+    if(Stack_IS_EMPTY(My_Stack)==STACK_EMPTY)
     {
         return STACK_EMPTY;
     }
-    
-    if( (My_Stack->count) == (My_Stack->length) )
-    {
-        *item=*(My_Stack->head);
-        My_Stack->head--;
-    }
-    else
-    {
-        My_Stack->head--;
-        *item=*(My_Stack->head);
-    }
-    
-    My_Stack->count--;
 
+    My_Stack->head--;
+
+    *item=*(My_Stack->head);
+
+    My_Stack->count--;
     return STACK_OK;
 }
 
 Stack_Status Stack_Top		(Stack_Buf_t* My_Stack , element_type *item)
 {
     // For Validation
-    if(Stack_IS_EMPTY==STACK_EMPTY)
+    if(Stack_IS_EMPTY(My_Stack)==STACK_EMPTY)
     {
         return STACK_EMPTY;
     }
 
-    if(My_Stack->count==My_Stack->length)
-    {
-        *item=*(My_Stack->head);
-    }
-    else
-    {
-        *item=*(My_Stack->head-1);
-    }
+    *item=*(My_Stack->head-1);
 
     return STACK_OK;
 }
 
-Stack_Status Stack_SIZE		(Stack_Buf_t* My_Stack,uint32_t* Stack_Size)
+Stack_Status Stack_Size		(Stack_Buf_t* My_Stack,uint32_t* Stack_Size)
 {
     *Stack_Size = My_Stack->count;
     return STACK_OK;
@@ -87,18 +70,17 @@ Stack_Status Stack_SIZE		(Stack_Buf_t* My_Stack,uint32_t* Stack_Size)
 
 Stack_Status Stack_IS_FULL	(Stack_Buf_t* My_Stack)
 {
-    if( (My_Stack->count) == (My_Stack->length) )
+    if( My_Stack->count == My_Stack->length )
     {
         return STACK_FULL;
     }
-
     return STACK_OK;
-    
+
 }
 
 Stack_Status Stack_IS_EMPTY	(Stack_Buf_t* My_Stack)
 {
-    if( (My_Stack->count) == 0)
+    if( (My_Stack->count) == 0 )
     {
         return STACK_EMPTY;
     }
